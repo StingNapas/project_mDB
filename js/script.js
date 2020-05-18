d7f9ade55e03bdfb7d725028e89f45939c766096
Sergey <2sting2@mail.ru>
2020-05-18 13:22:48 +0300

    after first change(last)

--- js/script.js
+++ js/script.js
@@ -25,26 +25,21 @@ const movieDB = {
 };
 
 //1
-const promoAdv = document.querySelectorAll(".promo__adv > img");
-// console.dir(promoAdv);
-// promoAdv.forEach(element => {
-// 	element.remove();
-// });
+const promoAdv = document.querySelectorAll(".promo__adv img");
+promoAdv.forEach(element => {
+	element.remove();
+});
 
 //2
 const promoGenre = document.getElementsByClassName("promo__genre");
-// promoGenre[0].innerHTML = "драма";
 promoGenre[0].textContent = "драма";
-// console.dir(promoGenre);
 
 //3
 const promoBg = document.getElementsByClassName("promo__bg");
-// const promoBg = document.querySelectorAll(".promo__bg");
 promoBg[0].style.backgroundImage = "url('img/bg.jpg')";
 
 //4
 const promoItems = document.querySelectorAll(".promo__interactive-item");
-console.dir(promoItems);
 promoItems.forEach(item => item.remove());
 
 movieDB.movies.sort();
